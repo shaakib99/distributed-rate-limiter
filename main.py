e511bdcaf96216db8c5aa2cdf346f66541626f6f
Wahid Shakib <wsakib87@gmail.com>
2025-12-15 10:14:20 +0600

    added demo router in main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from .service.router import router as demo_router
 
 load_dotenv()
 app = FastAPI()
+
+routers = [demo_router]
+for r in routers:
+    app.include_router(r)
+
